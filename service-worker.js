59d27882950b0d6f276630a87313914c3296d7b9
Jan Bartel <barteljan@yahoo.de>
2021-09-16 12:51:43 +0200

    added usable buttons to start page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a4b66349a30f099467941aedf5054d1.js"
+  "/precache-manifest.778e4b0ad714f0f57697dfce7db41658.js"
 );
 
 self.addEventListener('message', (event) => {
